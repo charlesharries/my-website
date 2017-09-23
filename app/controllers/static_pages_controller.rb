a10a6ba815cb355f6717484ad6385f786e670bc6
Charles Harries <charlesgharries@gmail.com>
2017-09-23 20:36:13 +0100

    Add new profile pictures and logic for random profile picture

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,8 @@ class StaticPagesController < ApplicationController
   before_filter :disable_header, :disable_footer, only: [:home]
 
   def home
+    index = Random.rand(Dir.glob("app/assets/images/profile_pictures/*").length) + 1
+    @image = "prof-#{index}.jpg"
   end
 
 end
