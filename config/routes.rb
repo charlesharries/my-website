dd9660d1db02d538a50eede20a96448bf59af92d
Charles Harries <charlesgharries@gmail.com>
2018-01-06 21:42:52 +0000

    Change routing to remove expirments/opinion/blog

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get 'drafts',      to: 'posts#drafts'
-  get 'work',        to: 'posts#work'
-  get 'opinions',    to: 'posts#opinions'
-  get 'experiments', to: 'posts#experiments'
+  get 'blog',        to: 'posts#index'
   get 'contact',     to: 'static_pages#contact'
 
   devise_scope :user do
