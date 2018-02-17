d7cf670e561e03375d73ba53133f6b914f5cdaba
Charles Harries <charlesgharries@gmail.com>
2018-02-17 15:15:40 +0000

    Redirect www to non-www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :redirect_www
+
+  def redirect_www
+  	if request.host == 'www.charlesharri.es'
+  		redirect_to 'https://charlesharri.es'
+  	end
+  end
 
   def disable_header
     @disable_header = true
